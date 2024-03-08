f539398b907452e1ce78c7a114e91e520a46da89
Max Bradshaw <maxbshaw17@gmail.com>
2024-03-08 10:18:05 -0500

    More NLP stuff

--- article_api/LSA.py
+++ article_api/LSA.py
@@ -1,4 +1,22 @@
 from db_update.db_connector import *
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd
 
-headlines = get_all_headlines()
+headlines_tuple = get_all_headlines()
 
+headlines = []
+
+for headline in headlines_tuple:
+    headlines.append(headline[0])
+    
+"""
+for x in headlines:
+    print(x)
+    
+print(len(headlines))
+"""
+
+vectorizer = CountVectorizer()
+bag_of_words = vectorizer.fit_transform(headlines)
+
+print(bag_of_words)
