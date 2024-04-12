dff583512083c7cf60d922340f0395f24b97cec4
Jake Dorick <jdorick8@gmail.com>
2024-04-12 12:09:43 -0400

    Fixing the API

--- website/articles.js
+++ website/articles.js
@@ -1,8 +1,9 @@
-fetch('/articles_tickers_api/articles_api')
+console.log("Before fetch")
+fetch('http://127.0.0.1:5000/articles_tickers_api/articles_api')
+  console.log("After fetch")
   .then(response => response.json())
   .then(articles => {
     const dynamicArticlesContainer = document.getElementById('dynamic-articles');
-
     // Loop through the articles and display them on the page
     articles.forEach(article => {
       const articleElement = document.createElement('div');
